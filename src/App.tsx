8984e8c1ab115b2ae869dd45b1c49679e1a2e2fb
Julia Chen <julia9422002@gmail.com>
2023-07-21 15:39:51 +0800

    add WalletKitProvider and btn (#1)
    feature/suiwallet_connectButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { WalletKitProvider } from '@mysten/wallet-kit';
 
 import { generateLinks } from '@/Constants';
 import { Sidebar } from '@/Components';
@@ -8,19 +9,19 @@ import { Sidebar } from '@/Components';
 function App() {
   const links = useMemo(() => generateLinks(), []);
 
-  console.log(links)
-
   return (
-    <BrowserRouter>
-      <Sidebar isOpen={false} />
-      <Routes>
-        {
-          links.map((link) => {
-            return <Route {...link} />;
-          })
-        }
-      </Routes>
-    </BrowserRouter>
+    <WalletKitProvider>
+      <BrowserRouter>
+        <Sidebar isOpen={false} />
+        <Routes>
+          {
+            links.map((link) => {
+              return <Route {...link} />;
+            })
+          }
+        </Routes>
+      </BrowserRouter>
+    </WalletKitProvider>
   )
 }
 
---
+++ src/Components/SuiWalletConnectButton/index.tsx
@@ -0,0 +1,22 @@
+import { ConnectButton, useWalletKit } from '@mysten/wallet-kit';
+import { formatAddress } from '@mysten/sui.js';
+
+import * as styles from './index.styles';
+
+const SuiWalletConnectButton = () => {
+  const ConnectToWallet = () => {
+    const { currentAccount } = useWalletKit();
+
+    return (
+      <ConnectButton
+          className={styles.buttonStyle}
+          connectText={'Connect Wallet'}
+          connectedText={currentAccount && `Connected: ${formatAddress(currentAccount.address)}`}
+      />
+    );
+  }
+
+  return <ConnectToWallet />;
+};
+
+export default SuiWalletConnectButton;
