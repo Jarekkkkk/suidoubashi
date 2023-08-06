dde72c0e3b4b7adb9035086d9c4cb0d9006517e7
Julia Chen <julia9422002@gmail.com>
2023-08-06 16:22:56 +0800

    add svg icon

---
+++ src/Assets/icon/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { ReactComponent as SwapIcon  } from "./icon_swap.svg";
+import { ReactComponent as PoolIcon  } from '@/Assets/icon/icon_pool.svg';
+import { ReactComponent as VestIcon  } from '@/Assets/icon/icon_vest.svg';
+import { ReactComponent as VoteIcon  } from '@/Assets/icon/icon_vote.svg';
+import { ReactComponent as RewardsIcon  } from '@/Assets/icon/icon_rewards.svg';
+
+export const Icon = {
+  SwapIcon,
+  PoolIcon,
+  VestIcon,
+  VoteIcon,
+  RewardsIcon,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
