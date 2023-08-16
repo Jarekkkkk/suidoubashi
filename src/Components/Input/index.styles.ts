80f242ebe4fa0f9651bb6c92042b36563110a32c
Julia Chen <julia9422002@gmail.com>
2023-08-16 17:37:58 +0800

    renew input style

--- src/Components/Input/index.styles.ts
+++ src/Components/Input/index.styles.ts
@@ -1,17 +1,6 @@
 import { css } from '@emotion/css'
 
 export const InputContent = css`
-  display: flex;
-  padding: 5px;
-  border-radius: 8px;
-  border: 1px solid #606266;
-  background-color: #fff;
-  overflow: hidden;
-  transition: 0.3s;
-
-  &:focus, &:focus-within, &:active {
-    border-color: #2977EC;
-  }
 
   svg {
     margin-right: 3px;
@@ -19,14 +8,19 @@ export const InputContent = css`
 
   input {
     margin: 0;
-    padding: 0;
-    border: none;
-    color: #606266;
-    font-size: 16px;
+    padding: 0 10px;
     width: 100%;
+    border-radius: 8px;
+    border: 1px solid #606266;
+    background-color: #fff;
+    box-shadow: none;
+    color: #2977EC;
+    font-size: 16px;
+    font-weight: bold;
 
-    &:focus {
-      border: none;
+    &:focus, &:focus-within, &:active {
+      border-color: #2977EC;
+      box-shadow: none;
       outline: none;
     }
 
