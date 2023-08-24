d2e5de9d19095e17e3f51d40ac3f81f4faf1c912
Jarekkkkk <86938582+Jarekkkkk@users.noreply.github.com>
2023-08-24 17:25:29 +0800

    [front_end] emptry

--- src/Components/Empty/index.tsx
+++ src/Components/Empty/index.tsx
@@ -1,7 +1,7 @@
 import { Icon } from '@/Assets/icon'
 import * as styles from './index.styles'
 
-const EmptyComponent = ({content}:{content: string}) => {
+const EmptyComponent = ({ content }: { content: string }) => {
   return (
     <div className={styles.emptyContainer}>
       <Icon.LogoIcon />
