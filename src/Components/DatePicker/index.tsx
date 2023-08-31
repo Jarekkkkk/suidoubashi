c7fd1ccf1c7ccb8708c985df9002cd94538093fb
Jarekkkkk <86938582+Jarekkkkk@users.noreply.github.com>
2023-08-31 11:59:00 +0800

    date picker validation

--- src/Components/DatePicker/index.tsx
+++ src/Components/DatePicker/index.tsx
@@ -8,6 +8,10 @@ interface Props {
   handleOnChange: (date: any) => void
 }
 
+function addDays(theDate: Date, days: number) {
+  return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000)
+}
+
 const DatePickerComponent = (props: Props) => {
   const { endDate, handleOnChange } = props
 
@@ -18,6 +22,8 @@ const DatePickerComponent = (props: Props) => {
       onChange={(e) => handleOnChange(e?.toDateString())}
       className={styles.datePickerComponent}
       dateFormat='yyyy/MM/dd'
+      minDate={new Date()}
+      maxDate={addDays(new Date(), 24 * 7)}
     />
   )
 }
