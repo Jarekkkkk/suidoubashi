f2dd3e2eed88276176d71b10696c8c7af2b3113a
Jarekkkkk <86938582+Jarekkkkk@users.noreply.github.com>
2023-09-15 00:42:50 +0800

    table tl tag no key attributions

--- src/Components/Table/index.tsx
+++ src/Components/Table/index.tsx
@@ -1,46 +1,87 @@
 import { Empty } from '@/Components'
 import * as styles from './index.styles'
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
-import { JSX } from 'react/jsx-runtime';
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+} from 'react'
+import { JSX } from 'react/jsx-runtime'
 
 type TableProps = {
-  data: Array<any>,
-  columns: any,
-  renderRow: any,
+  data: Array<any>
+  columns: any
+  renderRow: any
 }
 
 const Table = (props: TableProps) => {
-  const { data, columns, renderRow } = props;
+  const { data, columns, renderRow } = props
 
-  console.log('data', data)
-  return (
-    !data.length ? (
-      <Empty content='Oops! No Data.' />
-    ) : (
-      <div className={styles.TableWrapper}>
-        <table>
-          <thead>
-            <tr>
-              {columns.map((column: { id: Key | null | undefined; Header: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => (
-                <th  key={column.id}>{column.Header}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {renderRow.map((row: { id: Key | null | undefined; map: (arg0: (rowValue: any) => JSX.Element) => string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => (
+  return !data.length ? (
+    <Empty content='Oops! No Data.' />
+  ) : (
+    <div className={styles.TableWrapper}>
+      <table>
+        <thead>
+          <tr>
+            {columns.map(
+              (column: {
+                id: Key | null | undefined
+                Header:
+                  | string
+                  | number
+                  | boolean
+                  | ReactElement<any, string | JSXElementConstructor<any>>
+                  | Iterable<ReactNode>
+                  | ReactPortal
+                  | null
+                  | undefined
+              }) => (
+                <th key={column.id}>{column.Header}</th>
+              ),
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {renderRow.map(
+            (row: {
+              id: Key | null | undefined
+              map: (
+                arg0: (rowValue: any) => JSX.Element,
+              ) =>
+                | string
+                | number
+                | boolean
+                | ReactElement<any, string | JSXElementConstructor<any>>
+                | Iterable<ReactNode>
+                | ReactPortal
+                | null
+                | undefined
+            }) => (
               <tr key={row.id}>
-                {
-                  row.map((rowValue: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined) => (
-                    <td>{rowValue}</td>
-                  ))
-                }
+                {row.map(
+                  (
+                    rowValue:
+                      | string
+                      | number
+                      | boolean
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | Iterable<ReactNode>
+                      | ReactPortal
+                      | null
+                      | undefined,
+                  ) => (
+                    <td key={row.id}>{rowValue}</td>
+                  ),
+                )}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
-  );
-};
+            ),
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
-export default Table;
+export default Table
