0b13553568cb5ebdbdded95bdf7ed00d7e7d59a3
Jarekkkkk <86938582+Jarekkkkk@users.noreply.github.com>
2023-09-29 20:51:18 +0800

    countdown time

--- src/Presentations/Vote/index.tsx
+++ src/Presentations/Vote/index.tsx
@@ -36,6 +36,38 @@ const VotePresentation = () => {
     rewardsData,
   } = useVoteContext()
 
+  const calculateCountdown = () => {
+    const now = new Date()
+    const currentDayOfWeek = now.getUTCDay() // 0 (Sunday) to 6 (Saturday)
+
+    // Calculate the number of days until the next Wednesday
+    const daysUntilWednesday = (3 - currentDayOfWeek + 7) % 7
+
+    // Create a target date for the next Wednesday at 23:59 UTC
+    const targetDate = new Date(
+      Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate() + daysUntilWednesday,
+        23,
+        59,
+        0,
+      ),
+    )
+
+    // Calculate the time difference
+    // @ts-ignore
+    const timeDifference = targetDate - now
+
+    // Calculate hours, minutes, and seconds
+    const days = Math.floor((timeDifference / (1000 * 60 * 60 * 24)) % 7)
+    const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24)
+    const minutes = Math.floor((timeDifference / (1000 * 60)) % 60)
+    const seconds = Math.floor((timeDifference / 1000) % 60)
+
+    return { days, hours, minutes, seconds }
+  }
+
   const [totalVoting, setTotalVoting] = useState<any>({})
 
   const { setting, currentNFTInfo } = usePageContext()
@@ -125,6 +157,20 @@ const VotePresentation = () => {
     }
   }, [currentNFTInfo])
 
+  const [countdown, setCountdown] = useState(calculateCountdown())
+
+  console.log('coountdown',countdown)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(calculateCountdown())
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   const data = [{ id: 1 }, { id: 2 }]
 
   if (fetching)
@@ -167,7 +213,6 @@ const VotePresentation = () => {
       Header: 'Weights',
     },
   ]
-
 
   const renderRow = (
     columns: any[],
@@ -299,7 +344,7 @@ const VotePresentation = () => {
             <div className={styles.infoTitle}>Epoch 1</div>
             <div>
               <div>Finished in </div>
-              <div className={styles.yellowText}>6d 23h 30s </div>
+              <div className={styles.yellowText}>{`${countdown.days}d ${countdown.hours}h ${countdown.minutes}m ${countdown.seconds}s`}</div>
             </div>
           </div>
         </div>
